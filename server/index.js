0ce8a8ff1e97832fe7bd939dbefc7e0869889ca5
Roman Budde <roman_budde@hotmail.com>
2023-07-25 15:01:50 -0300

    allowing cors requests at server index

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const ngrok_auth_token = process.env.NGROK_TOKEN;
 process.env.TZ = 'America/Sao_Paulo';
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(flash())
 
