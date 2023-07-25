ee418f55975a97fb04c1b56a2881c11e3bebf47a
Roman Budde <roman_budde@hotmail.com>
2023-07-25 09:45:06 -0300

    removed unnecessary code from server/index.js, and added prod credentials to db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,18 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "123456",
+//     host: "db",
+//     port: "5432",
+//     database: "db_cuidadores"
+// });
+
+// prod credentials
 const pool = new Pool({
-    user: "postgres",
-    password: "123456",
-    host: "db",
+    user: "radmin",
+    password: "B5qPiSbzg459W1yZAIZBG63dpI0uBpHj",
+    host: "dpg-civk1aunqql48o6k1at0-a.oregon-postgres.render.com",
     port: "5432",
     database: "db_cuidadores"
 });
